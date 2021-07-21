b8ffdde808cff18ad6ee029cd14a39b765b14fd7
Damian Tarnawsky <damian@ionic.io>
2021-07-21 11:58:37 -0700

    Initial Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AppRate } from '@ionic-enterprise/app-rate/ngx';
+
+@NgModule({
+  declarations: [AppComponent],
+  entryComponents: [],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  providers: [
+    AppRate,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ src/app/home/home.page.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { AppRate } from '@ionic-enterprise/app-rate/ngx';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  constructor(private appRate: AppRate) { }
+
+  public rate(): void {
+    // or, override the whole preferences object
+    this.appRate.preferences = {
+      usesUntilPrompt: 3,
+      storeAppURL: {
+        ios: '<app_id>',
+        android: 'market://details?id=<package_name>',
+        windows: 'ms-windows-store://review/?ProductId=<store_id>'
+      }
+    }
+
+    this.appRate.promptForRating(true);
+  }
+
+}
